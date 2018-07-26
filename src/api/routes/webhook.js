cc881cc9770606e97977ad14e205297377f975d6
GuilhermeHenrique_SMN <guilherme.henrique@smn.com.br>
2018-07-26 07:58:42 -0300

    testando TravisCI

--- src/api/routes/webhook.js
+++ src/api/routes/webhook.js
@@ -1,14 +1,14 @@
-const exec = require('child_process').exec;
-
-module.exports = (app) => {
-    app.route('/webhook').post((req, res) => {
-        let command = 'git pull';
-
-        exec(command, (err, out) => {
-            if (err) console.error(err);
-
-            console.log(out);
-            return res.status(200).json({content: out})
-        })
-    })
-};
+// const exec = require('child_process').exec;
+//
+// module.exports = (app) => {
+//     app.route('/webhook').post((req, res) => {
+//         let command = 'git pull';
+//
+//         exec(command, (err, out) => {
+//             if (err) console.error(err);
+//
+//             console.log(out);
+//             return res.status(200).json({content: out})
+//         })
+//     })
+// };
